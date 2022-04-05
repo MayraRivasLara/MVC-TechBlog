8895052c7991c4f830edd129f3aa29f189b3e38b
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-04-05 17:11:03 +0800

    loging end poit working

--- routes/web/home.js
+++ routes/web/home.js
@@ -1,9 +1,16 @@
 const router = require("express").Router();
-const { Blog, User } = require("../../models");
+const { Router } = require("express");
+const { Blog, User, Comment } = require("../../models");
 
+//
+
+router.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+// getting all blogs
 router.get("/home", (req, res) => {
-  
-    Blog.findAll({
+  Blog.findAll({
     include: [
       {
         model: User,
@@ -15,15 +22,92 @@ router.get("/home", (req, res) => {
     console.log(blogsJson);
     res.render("blogs", {
       blogs: blogsJson,
+      logged_in: req.session.loggedIn,
     });
   });
 });
 
-router.get("/login", (req, res) => {
-//   if (req.session.loggedIn)
-    res.render("login", {
-      aa: "<p>1234</p>",
+// getting one blog
+router.get("/blog/:id", (req, res) => {
+  Blog.findByPk({
+    include: [
+      {
+        model: User,
+      },
+    ],
+  }).then((blogs) => {
+    const blogsJson = blogs.map((blog) => blog.toJSON());
+
+    console.log(blogsJson);
+    res.render("blogs", {
+      blogs: blogsJson,
+      logged_in: req.session.loggedIn,
     });
+  });
+});
+
+// login endpoint
+router.get("/login", (req, res) => {
+  // check  session and redirect to the homepage if exists
+  if (req.session.loggedIn) {
+    res.redirect("/home");
+    return;
+  }
+  res.render("login", {
+    logged_in: req.session.loggedIn,
+  });
+});
+
+router.post("/login", async (req, res) => {
+  console.log("test", req.body);
+
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res.render("login", {
+        message: "Incorrect email or password, please try again",
+        logged_in: req.session.loggedIn,
+      });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.render("login", {
+        message: "Incorrect email or password, please try again",
+        logged_in: req.session.loggedIn,
+      });
+      return;
+    }
+    req.session.save(() => {
+      req.session.user_id = userData.isSoftDeleted;
+      req.session.loggedIn = true;
+
+      res.redirect("/");
+    });
+  } catch (err) {
+    res.status(400).json(error);
+  }
+});
+
+// signup endpoint
+router.get("/signup", (req, res) => {
+  // check session and redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect("/home");
+    return;
+  }
+  res.render("signup", { logged_in: req.session.loggedIn });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy();
+  }
+
+  res.redirect("/home");
 });
 
 // router.get("/dashboard", (req, res) => {
--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 // requesting all files
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./routes/web/home');
+const express = require("express");
+const session = require("express-session");
+const routes = require("./routes/web/home");
+const path = require("path");
+const exphbs = require("express-handlebars");
 
-const sequelize = require('./config/connect');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connect");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+const models = require("./models");
 
 // creating my app
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create();
-
 // Creating session configuration
 const sessionConfig = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.use(session(sessionConfig));
+const hbs = exphbs.create(); //({ helpers }) adentro de los parentesis.
 
 // link handlebars
 app.engine("handlebars", hbs.engine);
@@ -34,14 +35,10 @@ app.set("view engine", "handlebars");
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log("Now listening http://localhost:" + PORT));
-  });
-  
-
-
-
-
+  app.listen(PORT, () => console.log("Now listening http://localhost:" + PORT));
+});
